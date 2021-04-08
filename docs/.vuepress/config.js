23db86aa9dbb942574a9b67c5e0b3f7465e21af0
shibin <2263337973@qq.com>
2021-04-08 18:43:26 +0800

    feat: 🎸 note

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -2,5 +2,23 @@ module.exports = {
     base:'/pmp-notes',
     dest:'./dist',
     title: 'PMP学习笔记',
-    description: 'PMP学习笔记'
+    description: 'PMP学习笔记',
+    themeConfig: {
+        nav: [{
+                text: '首页',
+                link: '/'
+            },
+            {
+                text: 'Github',
+                link: 'https://github.com/shibin-cli/pmp-notes'
+            },
+        ],
+        sidebar: [
+            ['/1', '引论'],
+        ]
+    },
+    plugins: ['@vuepress/pwa', {
+        serviceWorker: true,
+        updatePopup: true
+    }],
   }
