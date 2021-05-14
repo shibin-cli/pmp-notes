8a5f15e931c6360835e26611bf07d411505a11c9
shibin <2263337973@qq.com>
2021-05-14 13:46:53 +0800

    ci: 🎡 test

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -64,8 +64,15 @@ module.exports = {
             }
         ]
     },
-    plugins: ['@vuepress/pwa', {
-        serviceWorker: true,
-        updatePopup: true
-    }]
+    plugins: {
+        plugins: {
+            '@vuepress/pwa': {
+               serviceWorker: true,
+               updatePopup: {
+                 message: "New content is available.",
+                 buttonText: "Refresh"
+               }
+             }
+          }
+    }
 }
